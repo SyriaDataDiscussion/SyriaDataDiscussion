74c30f28c8db0c6af41f30de22bbc7e285b6fb6e
SyriaDataDiscussion <SyriaDataDiscussion@users.noreply.github.com>
2014-07-17 11:29:28 -0400

    Update view.php

--- view.php
+++ view.php
@@ -12,8 +12,7 @@
 		<script src="filter-rows.js"></script>		
 		<script src="sort-rows.js"></script>
 		<script src="view-rows.js"></script>
-		<!-- Adding Google Analytics-->
-		<script type="text/javascript">var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-46301789-1']); _gaq.push(['_trackPageview']); (function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();</script>		
+	</head>
 		<body>
 		<div id = "top">
 			<div id = "ribbon">
